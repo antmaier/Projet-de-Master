7aa2806d5a0f0f1e734a81e3e53f1aeacb2e20ab
Antoine Maier <antoine.maier@outlook.com>
2024-01-16 17:18:04 +0100

    Changed grid to VBox and _add_parameter directly add parameter to dictionary + tested

--- gui.py
+++ gui.py
@@ -1,7 +1,9 @@
-from ipywidgets import GridspecLayout, BoundedFloatText, BoundedIntText, Label, HTML
-from sys import maxsize, float_info
+import ipywidgets as widgets
+from ipywidgets import Label, HTML, FloatLogSlider, IntSlider, HBox
+from sys import maxsize
 
 
+# TODO update docstrings
 class GUI():
     """A GUI for the translocation model experiments.
 
@@ -29,13 +31,11 @@ class GUI():
         IPython.display.display(gui.grid)
     """
 
-    def __init__(self, n_rows):
-        self.n_rows = n_rows
-        self.n_cols = 3
-        self.grid = GridspecLayout(self.n_rows, self.n_cols)
+    def __init__(self):
         self.parameters = {}
+        self.gui = widgets.VBox()
 
-    def add_general_parameters(self, row: int = 0) -> None:
+    def add_general_parameters(self) -> None:
         """Add write/read interface for general physical parameters (9 rows).
 
         General parameters are the physical parameters common to all models.
@@ -58,55 +58,46 @@ class GUI():
         7: ATP hydrolysis rate (k_h)
         8: ATP synthesis rate (k_s)
         """
-        self.grid[row, :] = HTML(value="<b>General Physical Parameters</b>")
+        self.gui.children += (HTML(value="<b>General Physical Parameters</b>"),)
 
-        atp_adp_ratio = self._add_parameter(
-            row + 1, "[ATP]/[ADP]", "ATP/ADP concentration ratio", value=10.0)
+        self._add_parameter(
+            'atp_adp_ratio', "[ATP]/[ADP]", "ATP/ADP concentration ratio", 
+            value=10.0)
         
-        equilibrium_atp_adp_ratio = self._add_parameter(
-            row + 2, "([ATP]/[ADP])|eq.", 
+        self._add_parameter(
+            'equilibrium_atp_adp_ratio', "([ATP]/[ADP])|eq.", 
             "Equilibrium ATP/ADP concentration ratio", value=0.01)
         
-        K_d_atp = self._add_parameter(
-            row + 3, "K_d^ATP", "Protomer-ATP dissociation constant", value=0.1)
+        self._add_parameter(
+            'K_d_atp', "K_d^ATP", "Protomer-ATP dissociation constant", 
+            value=0.1)
         
-        K_d_adp = self._add_parameter(
-            row + 4, "K_d^ADP", "Protomer-ADP dissociation constant")
+        self._add_parameter(
+            'K_d_adp', "K_d^ADP", "Protomer-ADP dissociation constant")
         
-        k_DT = self._add_parameter(
-            row + 5, "k_DT", "Effective ADP->ATP exchange rate")
+        self._add_parameter('k_DT', "k_DT", "Effective ADP->ATP exchange rate")
         
         def compute_k_TD():
-            return (k_DT.value * K_d_atp.value / K_d_adp.value 
-                    / atp_adp_ratio.value)
-        k_TD = self._add_parameter(
-            row + 6, "k_TD", "Effective ATP->ADP exchange rate "\
+            return (self.parameters['k_DT'].value 
+                    * self.parameters['K_d_atp'].value 
+                    / self.parameters['K_d_adp'].value 
+                    / self.parameters['atp_adp_ratio'].value)
+        self._add_parameter(
+            'k_TD', "k_TD", "Effective ATP->ADP exchange rate "\
                 "(constrained by ATP<->ADP exchange model)", 
             value=compute_k_TD(), type='constrained')
         def change_k_TD(_): 
-            k_TD.value = str(compute_k_TD())
-        k_DT.observe(change_k_TD, names='value')
-        K_d_atp.observe(change_k_TD, names='value')
-        K_d_adp.observe(change_k_TD, names='value')
-        atp_adp_ratio.observe(change_k_TD, names='value')
+            self.parameters['k_TD'].value = str(compute_k_TD())
+        self.parameters['k_DT'].observe(change_k_TD, names='value')
+        self.parameters['K_d_atp'].observe(change_k_TD, names='value')
+        self.parameters['K_d_adp'].observe(change_k_TD, names='value')
+        self.parameters['atp_adp_ratio'].observe(change_k_TD, names='value')
 
-        k_h = self._add_parameter(row + 7, "k_h", "ATP hydrolysis rate")
+        self._add_parameter('k_h', "k_h", "ATP hydrolysis rate")
 
-        k_s = self._add_parameter(
-            row + 8, "k_s", "ATP synthesis rate", value=0.1)
+        self._add_parameter('k_s', "k_s", "ATP synthesis rate", value=0.1)
 
-        self.parameters.update({
-            "atp_adp_ratio": atp_adp_ratio,
-            "equilibrium_atp_adp_ratio": equilibrium_atp_adp_ratio,
-            "K_d_atp": K_d_atp,
-            "K_d_adp": K_d_adp,
-            "k_DT": k_DT,
-            "k_TD": k_TD,
-            "k_h": k_h,
-            "k_s": k_s,
-        })
-
-    def add_sc2r_parameters(self, row: int) -> None:
+    def add_sc2r_parameters(self) -> None:
         """Add write/read interface for SC2R parameters (3 rows).
 
         Add inplace the parameters to the given grid and the 
@@ -122,27 +113,28 @@ class GUI():
         1: Translocation up rate (k_up)
         2: Translocation down rate (k_down) (constrained by the detailed balance)
         """
-        self.grid[row, :] = HTML(value="<b>Sequential Clockwise/2-Residue "\
-                                 "Step Model Physical Parameters</b>")
+        self.gui.children += (HTML(
+            value="<b>SC2R Model Physical Parameters</b>"),)
         
-        k_up = self._add_parameter(row + 1, "k_↑", "Translocation up rate")
+        self._add_parameter('k_up', "k_↑", "Translocation up rate")
         
         def compute_k_down():
             return (
-                (self.parameters["k_h"].value * k_up.value 
-                 * self.parameters["k_DT"].value)
-                / (self.parameters["k_s"].value 
-                   * float(self.parameters["k_TD"].value)) # Because k_TD is a Label
-                * (self.parameters["equilibrium_atp_adp_ratio"].value 
-                   / self.parameters["atp_adp_ratio"].value)
+                (self.parameters['k_h'].value 
+                 * self.parameters['k_up'].value 
+                 * self.parameters['k_DT'].value)
+                / (self.parameters['k_s'].value 
+                   * float(self.parameters['k_TD'].value)) # Because k_TD is a Label
+                * (self.parameters['equilibrium_atp_adp_ratio'].value 
+                   / self.parameters['atp_adp_ratio'].value)
             )
-        k_down = self._add_parameter(
-            row + 2, "k_↓", 
+        self._add_parameter(
+            'k_down', "k_↓", 
             "Translocation down rate (constrained by the detailed balance)",
             value=compute_k_down(), type='constrained')
         def change_k_down(_):
-            k_down.value = str(compute_k_down())
-        k_up.observe(change_k_down, names='value')
+            self.parameters['k_down'].value = str(compute_k_down())
+        self.parameters['k_up'].observe(change_k_down, names='value')
         self.parameters['atp_adp_ratio'].observe(change_k_down, names='value')
         self.parameters['equilibrium_atp_adp_ratio'].observe(change_k_down, 
                                                              names='value')
@@ -150,12 +142,7 @@ class GUI():
         self.parameters['k_s'].observe(change_k_down, names='value')
         self.parameters['k_DT'].observe(change_k_down, names='value')
 
-        self.parameters.update({
-            "k_up": k_up,
-            "k_down": k_down,
-        })
-
-    def add_disc_spiral_parameters(self, row: int) -> None:
+    def add_disc_spiral_parameters(self) -> None:
         """Add write/read interface for Disc-Spiral parameters (8 rows).
 
         Add inplace the parameters to the given grid and the 
@@ -177,130 +164,140 @@ class GUI():
         7: Spiral->disc down translocation rate (k_extended_to_flat_down) (
             constrained by the detailed balance)
         """
-        self.grid[row, 0] = HTML(
-            value="<b>Disc-Spiral Model Physical Parameters</b>")
+        self.gui.children += (HTML(
+            value="<b>Disc-Spiral Model Physical Parameters</b>"),)
         
-        n_protomers = self._add_parameter(
-            row + 1, "n_protomers", "Number of protomers", value=6, type='int', 
-            min=1)
+        self._add_parameter(
+            'n_protomers', "n_protomers", "Number of protomers", value=6, 
+            type='int', min=1, max=10)
         
         def compute_k_h_bar(): 
-            return n_protomers.value * self.parameters["k_h"].value
+            return (self.parameters['n_protomers'].value 
+                    * self.parameters['k_h'].value)
         k_h_bar = self._add_parameter(
-            row + 2, "ꝁ_h", "Effective ATP hydrolysis rate", 
+            'k_h_bar', "ꝁ_h", "Effective ATP hydrolysis rate", 
             value=compute_k_h_bar(), type='constrained')
         def change_k_h_bar(_):
             k_h_bar.value = str(compute_k_h_bar())
-        n_protomers.observe(change_k_h_bar, names='value')
+        self.parameters['n_protomers'].observe(change_k_h_bar, names='value')
         self.parameters['k_h'].observe(change_k_h_bar, names='value')
 
-        k_extended_to_flat_up = self._add_parameter(
-            row + 3, "k_⮫", "Spiral->disc up translocation rate")
+        self._add_parameter(
+            'k_extended_to_flat_up', "k_⮫", 
+            "Spiral->disc up translocation rate")
         
-        k_flat_to_extended_down = self._add_parameter(
-            row + 4, r"k_⮯", "Disc->spiral down translocation rate")
+        self._add_parameter(
+            'k_flat_to_extended_down', r"k_⮯", 
+            "Disc->spiral down translocation rate")
         
         def compute_k_flat_to_extended_down_bar():
-            return n_protomers.value * k_flat_to_extended_down.value
+            return (self.parameters['n_protomers'].value 
+                    * self.parameters['k_flat_to_extended_down'].value)
         k_flat_to_extended_down_bar = self._add_parameter(
-            row + 5, "ꝁ_⮯", "Effective disc->spiral down translocation rate", 
+            'k_flat_to_extended_down_bar', "ꝁ_⮯", 
+            "Effective disc->spiral down translocation rate", 
             value=compute_k_flat_to_extended_down_bar(), type='constrained')
         def change_k_flat_to_extended_down_bar(_):
             k_flat_to_extended_down_bar.value = str(
                 compute_k_flat_to_extended_down_bar())
-        n_protomers.observe(change_k_flat_to_extended_down_bar, names='value')
-        k_flat_to_extended_down.observe(change_k_flat_to_extended_down_bar,
-                                        names='value')
+        self.parameters['n_protomers'].observe(
+            change_k_flat_to_extended_down_bar, names='value')
+        self.parameters['k_flat_to_extended_down'].observe(
+            change_k_flat_to_extended_down_bar, names='value')
 
-        k_flat_to_extended_up = self._add_parameter(
-            row + 6, r"k_⮭", "Disc->spiral up translocation rate")
+        self._add_parameter(
+            'k_flat_to_extended_up', r"k_⮭", 
+            "Disc->spiral up translocation rate")
         
         def compute_k_extended_to_flat_down():
-            return ((self.parameters["k_h"].value * k_flat_to_extended_up.value 
+            return ((self.parameters["k_h"].value 
+                     * self.parameters['k_flat_to_extended_up'].value 
                      * self.parameters["k_DT"].value 
-                     * k_extended_to_flat_up.value)
+                     * self.parameters['k_extended_to_flat_up'].value)
                     / (self.parameters["k_s"].value 
                        * float(self.parameters["k_TD"].value) 
-                       * k_flat_to_extended_down.value)
+                       * self.parameters['k_flat_to_extended_down'].value)
                     * (self.parameters["equilibrium_atp_adp_ratio"].value 
                        / self.parameters["atp_adp_ratio"].value))
-        k_extended_to_flat_down = self._add_parameter(
-            row + 7, r"k_⮩", "Spiral->disc down translocation rate "\
-                "(constrained by the detailed balance)",
+        self._add_parameter(
+            'k_extended_to_flat_down', r"k_⮩", "Spiral->disc down "\
+                "translocation rate (constrained by the detailed balance)",
             value=compute_k_extended_to_flat_down(), type='constrained')
         def change_k_extended_to_flat_down(_):
-            k_extended_to_flat_down.value = str(compute_k_extended_to_flat_down())
+            self.parameters['k_extended_to_flat_down'].value = str(
+                compute_k_extended_to_flat_down())
         self.parameters['k_h'].observe(change_k_extended_to_flat_down, 
                                        names='value')
         self.parameters['k_s'].observe(change_k_extended_to_flat_down, 
                                        names='value')
         self.parameters['k_DT'].observe(change_k_extended_to_flat_down, 
                                         names='value')
-        k_extended_to_flat_up.observe(change_k_extended_to_flat_down, 
-                                      names='value')
-        k_flat_to_extended_down.observe(change_k_extended_to_flat_down,
-                                        names='value')
-        k_flat_to_extended_up.observe(change_k_extended_to_flat_down, 
-                                      names='value')
-        self.parameters['atp_adp_ratio'].observe(change_k_extended_to_flat_down, 
-                                                 names='value')
+        self.parameters['k_extended_to_flat_up'].observe(
+            change_k_extended_to_flat_down, names='value')
+        self.parameters['k_flat_to_extended_down'].observe(
+            change_k_extended_to_flat_down, names='value')
+        self.parameters['k_flat_to_extended_up'].observe(
+            change_k_extended_to_flat_down, names='value')
+        self.parameters['atp_adp_ratio'].observe(
+            change_k_extended_to_flat_down, names='value')
         self.parameters['equilibrium_atp_adp_ratio'].observe(
             change_k_extended_to_flat_down, names='value')
-        
-        self.parameters.update({
-            "n_protomers": n_protomers,
-            "k_h_bar": k_h_bar,
-            "k_extended_to_flat_up": k_extended_to_flat_up,
-            "k_flat_to_extended_down": k_flat_to_extended_down,
-            "k_flat_to_extended_down_bar": k_flat_to_extended_down_bar,
-            "k_flat_to_extended_up": k_flat_to_extended_up,
-            "k_extended_to_flat_down": k_extended_to_flat_down,
-        })
 
     def add_defect_sc2r_parameters(self, row: int) -> None:
         pass # TODO
 
     def _add_parameter(
         self,
-        row: int,
+        var_name: str,
         symbol: str,
         desc: str,
         value: float = 1.0,
         type: str = 'float',
-        min: float = 0.0,
+        min: int = None,
+        max: int = None,
     ) -> None:
-        """Add a parameter to the grid.
+        """Add a parameter to the gui and widget to the parameters dictionary.
 
-        The parameter has a symbol, a value, and a description. If the parameter 
-        is not constrained, the value is a FloatText widget. If the parameter is
-        constrained, the value is a Label widget.
+        If type is 'float', the parameter is a FloatLogSlider with min/max 
+        exponents. If type is 'int', the parameter is an IntSlider with min/max
+        values. If type is 'constrained', the parameter is an HTML widget. The
+        dynamical update of the constrained parameter is not handled by this
+        method.
 
         Args:
-            row: The row to add the parameter to.
+            var_name: The name of the parameter attribute in the model.
             symbol: The LaTeX symbol of the parameter.
             desc: A description of the parameter.
             value: The initial value of the parameter.
             type: The type of the parameter. Either 'float', 'int' or 
                 'constrained'.
-            min: The minimum value of the parameter. Not relevant if type is
-                'constrained'.
+            min: The minimum value. If type is 'float', min is the minimum 
+                exponent, not min value. If type is not 'float', the min value 
+                is the minimum value.
+            max: The maximum value. If type is 'float', max is the maximum
+                exponent, not max value. If type is not 'float', the max value
+                is the maximum value.
         """
         type = type.lower()
+        if min is None:
+            min = -2 if type == 'float' else 0
+        if max is None:
+            max = 2 if type == 'float' else maxsize
         if type == 'float':
-            parameter = BoundedFloatText(
-                value=value, min=min, max=float_info.max, 
+            parameter = FloatLogSlider(
+                value=value, min=min, max=max, 
                 description=symbol + ':')
         elif type == 'int':
-            parameter = BoundedIntText(
-                value=value, min=min, max=maxsize, 
+            parameter = IntSlider(
+                value=value, min=min, max=max, 
                 description=symbol + ':')
         elif type == 'constrained':
             parameter = HTML(value=str(value), description=symbol + ':')
         else:
             raise ValueError(
                 "type must be either 'float', 'int' or 'constrained'")
-        self.grid[row, 0] = parameter
-        self.grid[row, 1:] = Label(value=desc)
-        return parameter
+        hbox = HBox([parameter, Label(value=desc)])
+        self.gui.children += (hbox,)
+        self.parameters.update({var_name: parameter})
 
 
